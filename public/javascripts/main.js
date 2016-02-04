90119772f35b9bbdd93fde1170dc2455dbcf2f97
rulotico <rulotico@gmail.com>
2016-02-03 19:53:21 -0600

    home

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,15 +1,15 @@
 
-// h_menu=$(".q").width();
-// $(".q").animate({height:h_menu}, 700);
-// $(".logo").css("height",h_menu);
+h_menu=$(".q").width();
+$(".q").animate({height:h_menu}, 700);
+$(".logo").css("height",h_menu);
 
 
-// $(".logo img, .menu").jAnimate('fadeIn');
+$(".logo img, .menu").jAnimate('fadeIn');
 
-// // $(".barmenu").jAnimate('zoomIn');
+// $(".barmenu").jAnimate('zoomIn');
 
-// $("#1").mouseover(function(){$("#1 img").jAnimate('bounceInUp');});
-// $("#2").mouseover(function(){$("#2 img").jAnimate('bounceInUp');});
-// $("#3").mouseover(function(){$("#3 img").jAnimate('bounceInUp');});
-// $("#4").mouseover(function(){$("#4 img").jAnimate('bounceInUp');});
-// $("#5").mouseover(function(){$("#5 img").jAnimate('bounceInUp');});
+$("#1").mouseover(function(){$("#1 img").jAnimate('bounceInUp');});
+$("#2").mouseover(function(){$("#2 img").jAnimate('bounceInUp');});
+$("#3").mouseover(function(){$("#3 img").jAnimate('bounceInUp');});
+$("#4").mouseover(function(){$("#4 img").jAnimate('bounceInUp');});
+$("#5").mouseover(function(){$("#5 img").jAnimate('bounceInUp');});
