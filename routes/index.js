cfd007930aecfdf003541d4febdd67e26606324d
rulotico <rulotico@gmail.com>
2016-02-07 17:19:38 -0600

    feat: pantallas principales y footer sin mapa

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Exaris' });
 });
 
+router.get('/aviation', function(req, res, next) {
+  res.render('aviation', { title: 'Aviation' });
+});
+
+router.get('/consulting', function(req, res, next) {
+  res.render('consulting', { title: 'Consulting' });
+});
+
+router.get('/energy', function(req, res, next) {
+  res.render('energy', { title: 'Energy' });
+});
+
+router.get('/medical', function(req, res, next) {
+  res.render('medical', { title: 'Medical' });
+});
+
+router.get('/safety', function(req, res, next) {
+  res.render('safety', { title: 'Safety' });
+});
 module.exports = router;
